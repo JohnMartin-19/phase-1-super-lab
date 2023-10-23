32f845b1eb93b3cdecfab771ba9dad1d6ace96dd
John <johnnkonge2020@gmail.com>
2023-10-23 06:06:59 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your classes here
+
+class tree{
+    constructor(species){
+         this.species = species
+    }
+    static definition(){
+        console.log("A tree is a perenial plant with elongated stem, trunk or leaves.")
+    }
+    
+}
+
+class decidious extends tree{
+    constructor(species, name){
+        super(species, name)
+        this.name = name
+    }
+    static decidious(){
+        console.log('decidious trees shed their leaves')
+    }
+}
+
+class evergreen extends tree{
+    constructor(species, name){
+        super(species,name)
+        this.species = species
+    }
+    static definition(){
+        console.log('Evergreens keep their leaves all year round')
+    }
+}
